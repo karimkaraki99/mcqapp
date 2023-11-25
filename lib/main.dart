4204065f9513d8ccbed58e617bed6f076968ea93
Karim <karimkaraki11@gmail.com>
2023-11-25 15:15:57 +0200

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MCQ App',
+      title: 'MCQ APP',
       home: Home() // calls the main page to display the application interface
     );
   }
