b61d1890ec090ae473b8e5471a9775c333857c5b
Nahles <alinahle794@gmail.com>
2023-11-25 15:23:43 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MCQ APP test test',
+      title: 'MCQ APP 1234',
       home: Home() // calls the main page to display the application interface
     );
   }
